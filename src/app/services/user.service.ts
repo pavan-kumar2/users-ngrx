b51ce2ddb8019a5e27b4195c60f1b38872a62140
Pavan Kumar <105868944+pavan-kumar2@users.noreply.github.com>
2026-03-08 11:49:58 +0530

    getusers method is created to call users api call

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private api = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private https: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.https.get<User[]>(this.api)
+  }
+
 }
